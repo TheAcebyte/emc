f1204cfa99f0295efd397ae6677ac9ff8a01f724
Acebyte <theacebyte@gmail.com>
2026-03-26 10:33:27 +0100

    Add debugging headers

---
+++ src/debug-motor.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <Arduino.h>
+#include "task.h"
+
+class DebugMotor : public Task {
+private:
+  static constexpr int ENA = 6;
+  static constexpr int IN1 = 9;
+  static constexpr int IN2 = 10;
+
+  static constexpr int ENB = 5;
+  static constexpr int IN3 = 7;
+  static constexpr int IN4 = 8;
+
+public:
+  void setup() override {
+    pinMode(ENA, OUTPUT);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(ENB, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+  }
+
+  void loop() override {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(ENA, 120);
+
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENB, 120);
+
+    delay(1000);
+  }
+};
