a81259bb3398dcdfd4683c79acba7d82bb00505a
Acebyte <theacebyte@gmail.com>
2026-03-27 15:57:50 +0100

    Add headers for lifting arm task

---
+++ src/lifting-arm-controller.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <Arduino.h>
+
+#define REMOTEXY_MODE__SOFTSERIAL
+
+#include <SoftwareSerial.h>
+
+#define REMOTEXY_SERIAL_RX 2
+#define REMOTEXY_SERIAL_TX 3
+#define REMOTEXY_SERIAL_SPEED 9600
+
+#include <RemoteXY.h>
+
+#pragma pack(push, 1)
+uint8_t const PROGMEM RemoteXY_CONF_PROGMEM[] = // 45 bytes V19
+    {255, 4,  0,  0,  0, 38, 0,  19,  0,  0,  0,  0,  31, 1,  106,
+     200, 1,  1,  3,  0, 5,  22, 125, 60, 60, 32, 2,  26, 31, 3,
+     19,  32, 16, 44, 3, 2,  26, 3,   68, 32, 16, 44, 3,  2,  26};
+
+struct {
+  int8_t joystickX;     // from -100 to 100
+  int8_t joystickY;     // from -100 to 100
+  uint8_t armSwitch;    // from 0 to 3
+  uint8_t clawSwitch;   // from 0 to 3
+  uint8_t connect_flag; // = 1 if wire connected, else = 0
+} RemoteXY;
+#pragma pack(pop)
+
+constexpr int MAX_JOYSTICK = 100;
+
+class LiftingArmController {
+private:
+  LiftingArmController() { RemoteXY_Init(); }
+
+public:
+  static LiftingArmController &getController() {
+    static LiftingArmController controller;
+    return controller;
+  }
+
+  LiftingArmController(const LiftingArmController &other) = delete;
+  LiftingArmController operator=(const LiftingArmController &other) = delete;
+
+  // void init() { RemoteXY_Init(); }
+  void step() { RemoteXYEngine.handler(); }
+  bool isConnected() { return RemoteXY.connect_flag; }
+
+  bool pushingArmForward() { return RemoteXY.armSwitch == 1; }
+  bool pushingArmBackward() { return RemoteXY.armSwitch == 2; }
+
+  bool pushingClawIn() { return RemoteXY.clawSwitch == 1; }
+  bool pushingClawOut() { return RemoteXY.clawSwitch == 2; }
+
+  double getJoystickX() { return (double)RemoteXY.joystickX / MAX_JOYSTICK; }
+  double getJoystickY() { return (double)RemoteXY.joystickY / MAX_JOYSTICK; }
+};
---
+++ src/lifting-arm.h
@@ -0,0 +1,127 @@
+#pragma once
+
+#include "HardwareSerial.h"
+#include "lifting-arm-controller.h"
+#include "task.h"
+#include "utils.h"
+#include <Arduino.h>
+#include <Servo.h>
+
+class LiftingArm : Task {
+private:
+  static constexpr int ENA = 6;
+  static constexpr int IN1 = 9;
+  static constexpr int IN2 = 10;
+
+  static constexpr int ENB = 5;
+  static constexpr int IN3 = 7;
+  static constexpr int IN4 = 8;
+
+  static constexpr int MAX_SPEED = 80;
+
+  static constexpr int ARM_SERVO = 11;
+  static constexpr int CLAW_SERVO = 12;
+
+  static constexpr int ARM_MIN_ANGLE = 0;
+  static constexpr int ARM_MAX_ANGLE = 90;
+  static constexpr int ARM_STEP_ANGLE = 5;
+
+  static constexpr int CLAW_MIN_ANGLE = 0;
+  static constexpr int CLAW_MAX_ANGLE = 90;
+  static constexpr int CLAW_STEP_ANGLE = 5;
+
+  static constexpr int TICK_DELAY = 20;
+
+  LiftingArmController &controller = LiftingArmController::getController();
+  Servo armServo;
+  Servo clawServo;
+
+  void setMotors(int leftSpeed, int rightSpeed) {
+    digitalWrite(IN1, leftSpeed > 0 ? HIGH : LOW);
+    digitalWrite(IN2, leftSpeed < 0 ? HIGH : LOW);
+    analogWrite(ENA, abs(leftSpeed));
+
+    digitalWrite(IN3, rightSpeed > 0 ? HIGH : LOW);
+    digitalWrite(IN4, rightSpeed < 0 ? HIGH : LOW);
+    analogWrite(ENB, abs(rightSpeed));
+  }
+
+  void stopMotors() { setMotors(0, 0); }
+
+  void nudgeArmForward() {
+    int newAngle = armServo.read() + ARM_STEP_ANGLE;
+    int clampedAngle = clamp(newAngle, ARM_MIN_ANGLE, ARM_MAX_ANGLE);
+    armServo.write(clampedAngle);
+  }
+
+  void nudgeArmBackward() {
+    int newAngle = armServo.read() - ARM_STEP_ANGLE;
+    int clampedAngle = clamp(newAngle, ARM_MIN_ANGLE, ARM_MAX_ANGLE);
+    armServo.write(clampedAngle);
+  }
+
+  void nudgeClawIn() {
+    int newAngle = clawServo.read() + CLAW_STEP_ANGLE;
+    int clampedAngle = clamp(newAngle, CLAW_MIN_ANGLE, CLAW_MAX_ANGLE);
+    clawServo.write(clampedAngle);
+  }
+
+  void nudgeClawOut() {
+    int newAngle = clawServo.read() - CLAW_STEP_ANGLE;
+    int clampedAngle = clamp(newAngle, CLAW_MIN_ANGLE, CLAW_MAX_ANGLE);
+    clawServo.write(clampedAngle);
+  }
+
+  void adjustMotors() {
+    double x = controller.getJoystickX();
+    double y = controller.getJoystickY();
+    double leftMultiplier = clamp(y + x, -1.0, 1.0);
+    double rightMultiplier = clamp(y - x, -1.0, 1.0);
+
+    int leftSpeed = leftMultiplier * MAX_SPEED;
+    int rightSpeed = rightMultiplier * MAX_SPEED;
+    setMotors(leftSpeed, rightSpeed);
+  }
+
+  void adjustServos() {
+    if (controller.pushingArmForward()) {
+      nudgeArmForward();
+    } else if (controller.pushingArmBackward()) {
+      nudgeArmBackward();
+    }
+
+    if (controller.pushingClawIn()) {
+      nudgeClawIn();
+    } else if (controller.pushingClawOut()) {
+      nudgeClawOut();
+    }
+  }
+
+public:
+  void setup() override {
+    pinMode(ENA, OUTPUT);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+
+    pinMode(ENB, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+
+    armServo.attach(ARM_SERVO);
+    clawServo.attach(CLAW_SERVO);
+    Serial.begin(9600);
+  }
+
+  void loop() override {
+    controller.step();
+    if (!controller.isConnected()) {
+      stopMotors();
+      return;
+    }
+
+    adjustMotors();
+    adjustServos();
+
+    delay(TICK_DELAY);
+  }
+};
