9a02081f9928ea53ac81f9216cff5c81b0f392c0
Acebyte <theacebyte@gmail.com>
2026-03-31 09:09:24 +0100

    Add confirmation on turns to maze solving logic

--- src/maze-solver/config.h
+++ src/maze-solver/config.h
@@ -3,25 +3,29 @@
 namespace MazeSolverConfig {
 // Pins
 constexpr int ENA = 6;
-constexpr int IN1 = 9;
-constexpr int IN2 = 10;
+constexpr int IN1 = 2;
+constexpr int IN2 = 4;
 constexpr int ENB = 5;
 constexpr int IN3 = 7;
 constexpr int IN4 = 8;
-constexpr int SERVO = 2;
+constexpr int SERVO = 9;
 constexpr int TRIG = 3;
-constexpr int ECHO = 4;
+constexpr int ECHO = 10;
 
 // Motors
+constexpr int ERROR_SCALE = 30;
 constexpr int MAX_SPEED = 255;
-constexpr int BASE_SPEED = 80;
+constexpr int BASE_SPEED = 100;
+constexpr int MOVE_DELAY = 350;
 constexpr int TURN_SPEED = 120;
-constexpr int MOVE_DELAY = 500;
-constexpr int TURN_DELAY = 500;
+constexpr int TURN_DELAY = 200;
+constexpr int TURN_THRESHOLD = 2;
 
 // Supersonic sensor
-constexpr int SERVO_DELAY = 200;
-constexpr int ECHO_TIMEOUT = 3000;
+constexpr int SERVO_DELAY = 400;
+constexpr int ECHO_TIMEOUT = 30000;
 constexpr double SOUND_SPEED = 0.034;
-constexpr double WALL_THRESHOLD = 20.0;
+constexpr double FRONT_WALL_THRESHOLD = 20.0;
+constexpr double SIDE_WALL_THRESHOLD = 30.0;
+constexpr double MAX_DISTANCE = 1000000.0;
 }; // namespace MazeSolverConfig
--- src/maze-solver/maze-solver.h
+++ src/maze-solver/maze-solver.h
@@ -4,34 +4,46 @@
 #include "task.h"
 #include "utils.h"
 #include <Arduino.h>
-#include <Servo.h>
+#include <PWMServo.h>
 
 using namespace MazeSolverConfig;
 
 struct WallState {
-  bool left;
-  bool front;
-  bool right;
+  double left;
+  double front;
+  double right;
+
+  bool emptyLeft() const { return left > SIDE_WALL_THRESHOLD; }
+  bool emptyFront() const { return front > FRONT_WALL_THRESHOLD; }
+  bool emptyRight() const { return right > SIDE_WALL_THRESHOLD; }
+  bool all() { return !emptyLeft() && !emptyFront() && !emptyRight(); }
 };
 
 class MazeSolver : Task {
 private:
-  Servo servo;
+  PWMServo servo;
+  int left = 0;
+  int right = 0;
 
   void setMotors(int leftSpeed, int rightSpeed) {
     int absoluteLeftSpeed = clamp(abs(leftSpeed), 0, MAX_SPEED);
     int absoluteRightSpeed = clamp(abs(rightSpeed), 0, MAX_SPEED);
 
-    digitalWrite(IN1, leftSpeed > 0 ? HIGH : LOW);
-    digitalWrite(IN2, leftSpeed < 0 ? HIGH : LOW);
-    analogWrite(ENA, absoluteLeftSpeed);
+    digitalWrite(IN3, leftSpeed > 0 ? HIGH : LOW);
+    digitalWrite(IN4, leftSpeed < 0 ? HIGH : LOW);
+    analogWrite(ENB, absoluteLeftSpeed);
 
-    digitalWrite(IN3, rightSpeed > 0 ? HIGH : LOW);
-    digitalWrite(IN4, rightSpeed < 0 ? HIGH : LOW);
-    analogWrite(ENB, absoluteRightSpeed);
+    digitalWrite(IN1, rightSpeed > 0 ? HIGH : LOW);
+    digitalWrite(IN2, rightSpeed < 0 ? HIGH : LOW);
+    analogWrite(ENA, absoluteRightSpeed);
   }
 
   void stopMotors() { setMotors(0, 0); }
+
+  void resetServo() {
+    servo.write(90);
+    delay(SERVO_DELAY);
+  }
 
   double measureDistance(int frontAngle = 0) {
     int angle = frontAngle + 90;
@@ -46,42 +58,94 @@ private:
 
     double time = pulseIn(ECHO, HIGH, ECHO_TIMEOUT);
     double distance = time * SOUND_SPEED / 2;
+    if (distance == 0) {
+      return MAX_DISTANCE;
+    }
+
     return distance;
   }
 
   WallState scanWalls() {
     WallState walls{
-        measureDistance(-90) < WALL_THRESHOLD,
-        measureDistance(0) < WALL_THRESHOLD,
-        measureDistance(90) < WALL_THRESHOLD,
+        measureDistance(90),
+        measureDistance(0),
+        measureDistance(-90),
     };
 
+    resetServo();
     return walls;
   }
 
-  void moveForward() {
-    setMotors(BASE_SPEED, BASE_SPEED);
+  double calculateError(WallState walls) {
+    double l = walls.left;
+    double r = walls.right;
+    if (l + r == 0) {
+      return 0;
+    }
+
+    double error = ERROR_SCALE * (r - l) / (l + r);
+    return error;
+  }
+
+  void moveForward(WallState walls) {
+    double error = 0;
+    if (!walls.emptyLeft() && !walls.emptyRight()) {
+      error = calculateError(walls);
+    }
+
+    int leftSpeed = BASE_SPEED + error;
+    int rightSpeed = BASE_SPEED - error;
+    setMotors(leftSpeed, rightSpeed);
     delay(MOVE_DELAY);
+    stopMotors();
   }
 
   void turnLeft() {
     setMotors(-TURN_SPEED, TURN_SPEED);
     delay(TURN_DELAY);
+    stopMotors();
   }
 
   void turnRight() {
     setMotors(TURN_SPEED, -TURN_SPEED);
     delay(TURN_DELAY);
+    stopMotors();
+  }
+
+  void turnAround() {
+    setMotors(TURN_SPEED, -TURN_SPEED);
+    delay(2 * TURN_DELAY);
+    stopMotors();
   }
 
   void adjustMotors(WallState walls) {
-    if (!walls.left) {
-      turnLeft();
-    } else if (!walls.front) {
-      moveForward();
-    } else {
-      turnRight();
+    if (walls.all()) {
+      left = right = 0;
+      turnAround();
+      return;
     }
+
+    if (walls.emptyLeft()) {
+      ++left;
+    } else {
+      left = 0;
+    }
+
+    if (walls.emptyRight()) {
+      ++right;
+    } else {
+      right = 0;
+    }
+
+    if (left >= TURN_THRESHOLD) {
+      turnLeft();
+      left = 0;
+    } else if (right >= TURN_THRESHOLD && !walls.emptyFront()) {
+      turnRight();
+      right = 0;
+    }
+
+    moveForward(walls);
   }
 
 public:
